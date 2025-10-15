50be508c5aaae5c8a5bca45d3d49aaf84f8f83b1
mark.davison <markdavison0+github@gmail.com>
2025-10-15 14:39:32 +1300

    Use configured config value

--- app/example/example.cpp
+++ app/example/example.cpp
@@ -2,11 +2,14 @@
 #include <iostream>
 
 #include <helsinki/Example/ExampleConfiguration.hpp>
+#include "ExampleConfig.hpp"
 
 int main(int, char**) {
 
 	auto example = ex::ExampleConfiguration();
-
+	
 	std::cout << "Hello World!" << std::endl;
+	std::cout << "Root Path: " << ex::ExampleConfig::RootPath << std::endl;
+
 	return EXIT_SUCCESS;
 }
