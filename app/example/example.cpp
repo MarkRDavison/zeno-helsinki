55e10d6674d7dfee08bb996948f685abe8d61052
mark.davison <markdavison0+github@gmail.com>
2025-10-15 14:57:30 +1300

    tutorial links and pre-reqs

--- app/example/example.cpp
+++ app/example/example.cpp
@@ -4,11 +4,14 @@
 #include <helsinki/Example/ExampleConfiguration.hpp>
 #include "ExampleConfig.hpp"
 
-int main(int, char**) {
-
+int main(int, char**)
+{
 	auto example = ex::ExampleConfiguration();
+
+	// https://docs.vulkan.org/tutorial/latest/01_Overview.html
+	// https://github.com/KhronosGroup/Vulkan-Tutorial/pull/119
+	// https://github.com/KhronosGroup/Vulkan-Tutorial/tree/03d05cf0aaf4b321254721b652eaecbcb95ea09b/en/Building_a_Simple_Engine
 	
-	std::cout << "Hello World!" << std::endl;
 	std::cout << "Root Path: " << ex::ExampleConfig::RootPath << std::endl;
 
 	return EXIT_SUCCESS;
