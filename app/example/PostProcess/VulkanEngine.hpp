1a88e8d3c7057916dfdb3fff0a3303a50b541e6e
mark.davison <markdavison0+github@gmail.com>
2025-10-23 00:21:05 +1300

    restructure

---
+++ app/example/PostProcess/VulkanEngine.hpp
@@ -0,0 +1,108 @@
+#pragma once
+
+#include "vk_types.hpp"
+#include "vkinit.hpp"
+#include "vk_images.hpp"
+#include "DeletionQueue.hpp"
+#include "vk_descriptors.hpp"
+#include "vk_pipelines.hpp"
+#include <VkBootstrap.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include "../ExampleConfig.hpp"
+
+#define ROOT_PATH(x) (std::string(ex::ExampleConfig::RootPath) + std::string(x))
+
+constexpr bool bUseValidationLayers = true;
+
+constexpr unsigned int FRAME_OVERLAP = 2;
+
+class VulkanEngine
+{
+public:
+    void run();
+
+    void initWindow();
+
+    static void framebufferResizeCallback(GLFWwindow* window, int, int);
+
+    void init();
+
+
+    void init_background_pipelines();
+
+    void init_pipelines();
+
+    void init_descriptors();
+
+    void init_vulkan();
+
+    void init_swapchain();
+
+    void init_commands();
+
+    void init_sync_structures();
+
+    void mainLoop();
+
+    void draw_background(VkCommandBuffer cmd);
+
+
+    void drawFrame();
+
+    void cleanup();
+
+    void create_swapchain(uint32_t width, uint32_t height);
+
+    void destroy_swapchain();
+
+private:
+    GLFWwindow* window{ nullptr };
+    bool framebufferResized = false;
+
+    bool _isInitialized{ false };
+    int _frameNumber{ 0 };
+    VkExtent2D _windowExtent{ 1024 , 768 };
+
+    VmaAllocator _allocator;
+
+    VkInstance _instance;// Vulkan library handle
+    VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+    VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+    VkDevice _device; // Vulkan device for commands
+    VkSurfaceKHR _surface;// Vulkan window surface
+
+
+    VkSwapchainKHR _swapchain;
+    VkFormat _swapchainImageFormat;
+
+    std::vector<VkImage> _swapchainImages;
+    std::vector<VkImageView> _swapchainImageViews;
+    VkExtent2D _swapchainExtent;
+
+
+    FrameData _frames[FRAME_OVERLAP];
+
+    FrameData& get_current_frame() { return _frames[_frameNumber % FRAME_OVERLAP]; };
+
+    VkQueue _graphicsQueue;
+    uint32_t _graphicsQueueFamily;
+
+
+    DeletionQueue _mainDeletionQueue;
+
+    AllocatedImage _drawImage;
+    VkExtent2D _drawExtent;
+    DescriptorAllocator globalDescriptorAllocator;
+
+    VkDescriptorSet _drawImageDescriptors;
+    VkDescriptorSetLayout _drawImageDescriptorLayout;
+
+    VkPipeline _gradientPipeline;
+    VkPipelineLayout _gradientPipelineLayout;
+
+    VkFence _immFence;
+    VkCommandBuffer _immCommandBuffer;
+    VkCommandPool _immCommandPool;
+};
--- app/example/PostProcess/vk_types.hpp
+++ app/example/PostProcess/vk_types.hpp
@@ -13,6 +13,7 @@
 #include <vulkan/vulkan.h>
 #include <vulkan/vk_enum_string_helper.h>
 #include <vk_mem_alloc.h>
+#include "DeletionQueue.hpp"
 
 #include <glm/mat4x4.hpp>
 #include <glm/vec4.hpp>
@@ -26,6 +27,16 @@
         }                                                               \
     } while (0)
 
+struct FrameData
+{
+    VkSemaphore _swapchainSemaphore;
+    VkSemaphore _renderSemaphore;
+    VkFence _renderFence;
+    VkCommandPool _commandPool;
+    VkCommandBuffer _mainCommandBuffer;
+
+    DeletionQueue _deletionQueue;
+};
 
 struct AllocatedImage
 {
