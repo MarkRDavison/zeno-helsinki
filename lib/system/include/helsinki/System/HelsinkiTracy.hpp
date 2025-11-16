c7b1d9b95a4a9cdcbe48a7508f3051d0bf2732c3
mark.davison <markdavison0+github@gmail.com>
2025-11-16 20:16:33 +1300

    Tracy is smart with empty defs

--- lib/system/include/helsinki/System/HelsinkiTracy.hpp
+++ lib/system/include/helsinki/System/HelsinkiTracy.hpp
@@ -1,11 +1,4 @@
 #pragma once
 
-
-#ifdef HELSINKI_TRACY_ENABLE
 #include <tracy/Tracy.hpp>
 #include <tracy/TracyVulkan.hpp>
-#else
-#define ZoneScoped
-#define ZoneScopedN(x)
-#define ZoneNameF(x,...)
-#endif
